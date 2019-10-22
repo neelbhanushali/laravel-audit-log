2b1fbea0097bef543305b0db29f31232c94856aa
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-10-22 22:39:55 +0530

    service provider added

---
+++ src/LaravelAuditLogServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NeelBhanushali\LaravelAuditLog;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAuditLogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
