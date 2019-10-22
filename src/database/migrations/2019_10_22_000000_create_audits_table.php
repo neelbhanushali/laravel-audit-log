1369a348ff90802bd6867ce8bf4c133f16e2e8cc
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-10-23 00:13:22 +0530

    migration and model for audit added

---
+++ src/database/migrations/2019_10_22_000000_create_audits_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuditsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('audit_trail', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('audit_trail');
+    }
+}
