f16580b2d87bd0fb3b19f5604dd705dbb618d56f
Neel Bhanushali <neal.bhanushali@gmail.com>
2019-10-25 22:40:52 +0530

    delete done

--- src/Traits/Auditable.php
+++ src/Traits/Auditable.php
@@ -3,6 +3,7 @@
 namespace NeelBhanushali\LaravelAuditLog\Traits;
 
 use NeelBhanushali\LaravelAuditLog\Models\Audit;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 trait Auditable
 {
@@ -86,6 +87,10 @@ trait Auditable
             $audit_trail->before_transaction = $before_transaction->after_transaction;
             $audit_trail->after_transaction = self::removeTimestamps($record, $record->attributes);
             $audit_trail->difference = self::removeTimestamps($record, $record->getChanges());
+            // restore fires `updating` and `updated` events internally
+            if(count($audit_trail->difference) == 0) {
+                return;
+            }
 
             $audit_trail->activity = 'update';
             $audit_trail->user_id = request()->user() ? request()->user()->id : null;
@@ -143,6 +148,56 @@ trait Auditable
 
             $audit_trail->save();
         });
+
+        $reflection = new \ReflectionClass(static::class);
+        if (in_array(SoftDeletes::class, $reflection->getTraitNames())) {
+            static::restored(function ($record) {
+                if (!defined(get_class($record) . '::AUDIT')) {
+                    $audit = [
+                        'relation' => 'self',
+                        'entity_id' => 'key',
+                        'entity_type' => get_class($record)
+                    ];
+                } else {
+                    $audit = $record::AUDIT;
+                }
+    
+                $audit_trail = new Audit;
+    
+                $audit_trail->entity_type = self::getValue($record, $audit, 'entity_type');
+                $audit_trail->entity_id = self::getValue($record, $audit, 'entity_id');
+                $audit_trail->relation = $audit['relation'];
+    
+                if ($audit_trail->relation != 'self') {
+                    $audit_trail->related_type =  self::getValue($record, $audit, 'entity_type');
+                    $audit_trail->related_id = self::getValue($record, $audit, 'entity_id');
+                    $audit_trail->entity_type = self::getValue($record, $audit, 'parent_type');
+                    $audit_trail->entity_id = self::getValue($record, $audit, 'parent_id');
+                }
+    
+                $before_transaction = Audit::where([
+                    'entity_type' => $audit_trail->entity_type,
+                    'entity_id' => $audit_trail->entity_id,
+                    'related_type' => $audit_trail->relation == 'self' ? null : $audit_trail->related_type,
+                    'related_id' => $audit_trail->relation == 'self' ? null : $audit_trail->related_id,
+                    'activity' => 'delete'
+                ])->latest()->first();
+    
+                $audit_trail->before_transaction = [];
+                $audit_trail->after_transaction = self::removeTimestamps($record, $record->attributes);
+                $audit_trail->difference = $before_transaction->after_transaction;
+    
+                $audit_trail->activity = 'restore';
+                $audit_trail->user_id = request()->user() ? request()->user()->id : null;
+    
+                $audit_trail->token = request()->bearerToken();
+                $audit_trail->ip = request()->ip();
+                $audit_trail->ua = request()->userAgent();
+                $audit_trail->url = url()->full();
+    
+                $audit_trail->save();
+            });
+        }
     }
 
     protected static function removeTimestamps($record, $data)
